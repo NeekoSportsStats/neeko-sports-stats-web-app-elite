9fc1c1868f1dce09576cdce4f046c5c3fbe0bfb5
NeekoSportsStats <matthew@neekostats.com.au>
2026-03-31 13:51:20 +1100

    Updated index.html

---
+++ dist/assets/square-BS6ON9fe.js
@@ -0,0 +1,11 @@
+import{P as e}from"./index-DxVQJ7cZ.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=e("SquareCheck",[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=e("Square",[["rect",{width:"18",height:"18",x:"3",y:"3",rx:"2",key:"afitv7"}]]);export{a as S,r as a};
